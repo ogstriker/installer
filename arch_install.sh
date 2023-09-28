943df537c17877f2f07835b44da58f35901364ac
ogstriker <146384997+ogstriker@users.noreply.github.com>
2023-09-28 18:07:41 -0300

    idk
    test

---
+++ arch_install.sh
@@ -0,0 +1,540 @@
+#!/bin/bash
+# and configures systemd as the init system (removing sysvinit).
+#
+# You should read through this script before running it in case you want to
+# make any modifications, in particular, the variables just below, and the
+# following functions:
+#
+#    install_packages - Customize packages installed in base system
+#                       (desktop environment, etc.)
+#    install_aur_packages - More packages after packer (AUR helper) is
+#                           installed
+#    set_netcfg - Preload netcfg profiles
+
+## CONFIGURE THESE VARIABLES
+## ALSO LOOK AT THE install_packages FUNCTION TO SEE WHAT IS ACTUALLY INSTALLED
+
+# Drive to install to.
+#DRIVE='/dev/sda'
+
+# Hostname of the installed machine.
+HOSTNAME='strikerhost'
+
+# Encrypt everything (except /boot).  Leave blank to disable.
+#ENCRYPT_DRIVE='TRUE'
+
+# Passphrase used to encrypt the drive (leave blank to be prompted).
+#DRIVE_PASSPHRASE='a'
+
+# Root password (leave blank to be prompted).
+ROOT_PASSWORD=''
+
+# Main user to create (by default, added to wheel group, and others).
+USER_NAME='striker'
+
+# The main user's password (leave blank to be prompted).
+USER_PASSWORD=''
+
+# System timezone.
+TIMEZONE='America/Montevideo'
+
+# Have /tmp on a tmpfs or not.  Leave blank to disable.
+# Only leave this blank on systems with very little RAM.
+TMP_ON_TMPFS='TRUE'
+
+KEYMAP='us'
+# KEYMAP='dvorak'
+
+# Choose your video driver
+# For Intel
+#VIDEO_DRIVER="i915"
+# For nVidia
+#VIDEO_DRIVER="nouveau"
+# For ATI
+#VIDEO_DRIVER="radeon"
+# For generic stuff
+#VIDEO_DRIVER="vesa"
+
+# Wireless device, leave blank to not use wireless and use DHCP instead.
+WIRELESS_DEVICE="wlan0"
+# For tc4200's
+#WIRELESS_DEVICE="eth1"
+
+setup() {
+    echo 'Installing base system'
+    install_base
+
+    echo 'Chrooting into installed system to continue setup...'
+    cp $0 /mnt/setup.sh
+    arch-chroot /mnt ./setup.sh chroot
+
+    if [ -f /mnt/setup.sh ]
+    then
+        echo 'ERROR: Something failed inside the chroot, not unmounting filesystems so you can investigate.'
+        echo 'Make sure you unmount everything before you try to run this script again.'
+    else
+        echo 'Unmounting filesystems'
+        unmount_filesystems
+        echo 'Done! Reboot system.'
+    fi
+}
+
+configure() {
+
+    echo 'Installing additional packages'
+    install_packages
+
+    echo 'Installing packer'
+    install_packer
+
+    echo 'Installing AUR packages'
+    install_aur_packages
+
+    echo 'Clearing package tarballs'
+    clean_packages
+
+    echo 'Updating pkgfile database'
+    update_pkgfile
+
+    echo 'Setting hostname'
+    set_hostname "$HOSTNAME"
+
+    echo 'Setting timezone'
+    set_timezone "$TIMEZONE"
+
+    echo 'Setting locale'
+    set_locale
+
+    echo 'Setting console keymap'
+    set_keymap
+
+    echo 'Setting hosts file'
+    set_hosts "$HOSTNAME"
+
+    echo 'Setting fstab'
+    set_fstab "$TMP_ON_TMPFS" "$boot_dev"
+
+    echo 'Setting initial modules to load'
+    set_modules_load
+
+    echo 'Configuring initial ramdisk'
+    set_initcpio
+
+    echo 'Setting initial daemons'
+    set_daemons "$TMP_ON_TMPFS"
+
+    echo 'Configuring sudo'
+    set_sudoers
+
+    if [ -n "$WIRELESS_DEVICE" ]
+    then
+        echo 'Configuring netcfg'
+        set_netcfg
+    fi
+
+    if [ -z "$ROOT_PASSWORD" ]
+    then
+        echo 'Enter the root password:'
+        stty -echo
+        read ROOT_PASSWORD
+        stty echo
+    fi
+    echo 'Setting root password'
+    set_root_password "$ROOT_PASSWORD"
+
+    if [ -z "$USER_PASSWORD" ]
+    then
+        echo "Enter the password for user $USER_NAME"
+        stty -echo
+        read USER_PASSWORD
+        stty echo
+    fi
+    echo 'Creating initial user'
+    create_user "$USER_NAME" "$USER_PASSWORD"
+
+    echo 'Building locate database'
+    update_locate
+
+    rm /setup.sh
+}
+
+install_base() {
+    echo 'Server = http://mirrors.kernel.org/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
+
+    pacstrap /mnt base base-devel
+}
+
+unmount_filesystems() {
+    umount -R /mnt/boot/EFI
+    umount -R /mnt
+}
+
+install_packages() {
+    local packages=''
+
+    # General utilities/libraries
+    packages+=' alsa-utils aspell-en chromium cpupower gvim mlocate net-tools ntp openssh p7zip pkgfile powertop python3 rfkill rsync sudo unrar unzip wget zip systemd-sysvcompat zsh'
+
+# DPS CONFIGURAR O OH-MY-ZSH
+
+    # Development packages
+    packages+=' cmake gdb git maven tcpdump valgrind wireshark-gtk'
+
+    # Netcfg
+    if [ -n "$WIRELESS_DEVICE" ]
+    then
+        packages+=' netcfg ifplugd dialog wireless_tools wpa_actiond wpa_supplicant'
+    fi
+
+    # Java stuff
+    packages+=' jdk17-openjdk jre17-openjdk'
+
+    # Libreoffice
+    packages+=' libreoffice-calc libreoffice-en-US libreoffice-gnome libreoffice-impress libreoffice-writer hunspell-en hyphen-en mythes-en'
+
+    # Misc programs
+    packages+=' vlc gparted'
+
+    # Xserver
+    packages+=' xorg-apps xorg-server xorg-xinit xterm'
+
+    # Sddm login manager
+    packages+=' sddm'
+
+    # Fonts
+    packages+=' ttf-dejavu ttf-liberation'
+
+    # For laptops
+    packages+=' xf86-input-synaptics'
+
+    # Extra packages for tc4200 tablet
+    #packages+=' ipw2200-fw xf86-input-wacom'
+
+    if [ "$VIDEO_DRIVER" = "i915" ]
+    then
+        packages+=' xf86-video-intel libva-intel-driver'
+    elif [ "$VIDEO_DRIVER" = "nouveau" ]
+    then
+        packages+=' xf86-video-nouveau'
+    elif [ "$VIDEO_DRIVER" = "radeon" ]
+    then
+        packages+=' xf86-video-ati'
+    elif [ "$VIDEO_DRIVER" = "vesa" ]
+    then
+        packages+=' xf86-video-vesa'
+    fi
+
+    pacman -Sy --noconfirm $packages
+}
+
+install_packer() {
+    mkdir /foo
+    cd /foo
+    curl https://aur.archlinux.org/yay.git
+    cd packer
+    makepkg -si --noconfirm --asroot
+
+    cd /
+    rm -rf /foo
+}
+
+install_aur_packages() {
+    mkdir /foo
+    export TMPDIR=/foo
+    packer -S --noconfirm lightly-git
+    packer -S --noconfirm q4wine-git
+    packer -S --noconfirm appimagelauncher
+    unset TMPDIR
+    rm -rf /foo
+}
+
+clean_packages() {
+    yes | pacman -Scc
+}
+
+update_pkgfile() {
+    pkgfile -u
+}
+
+set_hostname() {
+    local hostname="$1"; shift
+
+    echo "$hostname" > /etc/hostname
+}
+
+set_timezone() {
+    local timezone="$1"; shift
+
+    ln -sT "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
+}
+
+set_locale() {
+    echo 'LANG="en_US.UTF-8"' >> /etc/locale.conf
+    echo 'LC_COLLATE="C"' >> /etc/locale.conf
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+}
+
+set_keymap() {
+    echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+}
+
+set_hosts() {
+    local hostname="$1"; shift
+
+    cat > /etc/hosts <<EOF
+127.0.0.1 localhost.localdomain localhost $hostname
+::1       localhost.localdomain localhost $hostname
+EOF
+}
+
+set_fstab() {
+genfstab -U /mnt
+
+}
+
+set_initcpio() {
+    local vid
+
+    if [ "$VIDEO_DRIVER" = "i915" ]
+    then
+        vid='i915'
+    elif [ "$VIDEO_DRIVER" = "nouveau" ]
+    then
+        vid='nouveau'
+    elif [ "$VIDEO_DRIVER" = "radeon" ]
+    then
+        vid='radeon'
+    fi
+
+    local encrypt=""
+    if [ -n "$ENCRYPT_DRIVE" ]
+    then
+        encrypt="encrypt"
+    fi
+
+
+    # Set MODULES with your video driver
+    cat > /etc/mkinitcpio.conf <<EOF
+# vim:set ft=sh
+# MODULES
+# The following modules are loaded before any boot hooks are
+# run.  Advanced users may wish to specify all system modules
+# in this array.  For instance:
+#     MODULES="piix ide_disk reiserfs"
+MODULES="ext4 $vid"
+
+# BINARIES
+# This setting includes any additional binaries a given user may
+# wish into the CPIO image.  This is run last, so it may be used to
+# override the actual binaries included by a given hook
+# BINARIES are dependency parsed, so you may safely ignore libraries
+BINARIES=""
+
+# FILES
+# This setting is similar to BINARIES above, however, files are added
+# as-is and are not parsed in any way.  This is useful for config files.
+# Some users may wish to include modprobe.conf for custom module options
+# like so:
+#    FILES="/etc/modprobe.d/modprobe.conf"
+FILES=""
+
+# HOOKS
+# This is the most important setting in this file.  The HOOKS control the
+# modules and scripts added to the image, and what happens at boot time.
+# Order is important, and it is recommended that you do not change the
+# order in which HOOKS are added.  Run 'mkinitcpio -H <hook name>' for
+# help on a given hook.
+# 'base' is _required_ unless you know precisely what you are doing.
+# 'udev' is _required_ in order to automatically load modules
+# 'filesystems' is _required_ unless you specify your fs modules in MODULES
+# Examples:
+##   This setup specifies all modules in the MODULES setting above.
+##   No raid, lvm2, or encrypted root is needed.
+#    HOOKS="base"
+#
+##   This setup will autodetect all modules for your system and should
+##   work as a sane default
+#    HOOKS="base udev autodetect pata scsi sata filesystems"
+#
+##   This is identical to the above, except the old ide subsystem is
+##   used for IDE devices instead of the new pata subsystem.
+#    HOOKS="base udev autodetect ide scsi sata filesystems"
+#
+##   This setup will generate a 'full' image which supports most systems.
+##   No autodetection is done.
+#    HOOKS="base udev pata scsi sata usb filesystems"
+#
+##   This setup assembles a pata mdadm array with an encrypted root FS.
+##   Note: See 'mkinitcpio -H mdadm' for more information on raid devices.
+#    HOOKS="base udev pata mdadm encrypt filesystems"
+#
+##   This setup loads an lvm2 volume group on a usb device.
+#    HOOKS="base udev usb lvm2 filesystems"
+#
+##   NOTE: If you have /usr on a separate partition, you MUST include the
+#    usr, fsck and shutdown hooks.
+HOOKS="base udev autodetect modconf block keymap keyboard $encrypt lvm2 resume filesystems fsck"
+
+# COMPRESSION
+# Use this to compress the initramfs image. By default, gzip compression
+# is used. Use 'cat' to create an uncompressed image.
+#COMPRESSION="gzip"
+#COMPRESSION="bzip2"
+#COMPRESSION="lzma"
+#COMPRESSION="xz"
+#COMPRESSION="lzop"
+
+# COMPRESSION_OPTIONS
+# Additional options for the compressor
+#COMPRESSION_OPTIONS=""
+EOF
+
+    mkinitcpio -p linux
+}
+
+set_daemons() {
+    local tmp_on_tmpfs="$1"; shift
+
+    systemctl enable cpupower.service
+
+    if [ -n "$WIRELESS_DEVICE" ]
+    then
+        systemctl enable NetworkManager
+
+    if [ -z "$tmp_on_tmpfs" ]
+    then
+        systemctl mask tmp.mount
+    fi
+}
+
+set_sudoers() {
+    cat > /etc/sudoers <<EOF
+## sudoers file.
+##
+## This file MUST be edited with the 'visudo' command as root.
+## Failure to use 'visudo' may result in syntax or file permission errors
+## that prevent sudo from running.
+##
+## See the sudoers man page for the details on how to write a sudoers file.
+##
+
+##
+## Host alias specification
+##
+## Groups of machines. These may include host names (optionally with wildcards),
+## IP addresses, network numbers or netgroups.
+# Host_Alias	WEBSERVERS = www1, www2, www3
+
+##
+## User alias specification
+##
+## Groups of users.  These may consist of user names, uids, Unix groups,
+## or netgroups.
+# User_Alias	ADMINS = millert, dowdy, mikef
+
+##
+## Cmnd alias specification
+##
+## Groups of commands.  Often used to group related commands together.
+# Cmnd_Alias	PROCESSES = /usr/bin/nice, /bin/kill, /usr/bin/renice, \
+# 			    /usr/bin/pkill, /usr/bin/top
+
+##
+## Defaults specification
+##
+## You may wish to keep some of the following environment variables
+## when running commands via sudo.
+##
+## Locale settings
+# Defaults env_keep += "LANG LANGUAGE LINGUAS LC_* _XKB_CHARSET"
+##
+## Run X applications through sudo; HOME is used to find the
+## .Xauthority file.  Note that other programs use HOME to find   
+## configuration files and this may lead to privilege escalation!
+# Defaults env_keep += "HOME"
+##
+## X11 resource path settings
+# Defaults env_keep += "XAPPLRESDIR XFILESEARCHPATH XUSERFILESEARCHPATH"
+##
+## Desktop path settings
+# Defaults env_keep += "QTDIR KDEDIR"
+##
+## Allow sudo-run commands to inherit the callers' ConsoleKit session
+# Defaults env_keep += "XDG_SESSION_COOKIE"
+##
+## Uncomment to enable special input methods.  Care should be taken as
+## this may allow users to subvert the command being run via sudo.
+# Defaults env_keep += "XMODIFIERS GTK_IM_MODULE QT_IM_MODULE QT_IM_SWITCHER"
+##
+## Uncomment to enable logging of a command's output, except for
+## sudoreplay and reboot.  Use sudoreplay to play back logged sessions.
+# Defaults log_output
+# Defaults!/usr/bin/sudoreplay !log_output
+# Defaults!/usr/local/bin/sudoreplay !log_output
+# Defaults!/sbin/reboot !log_output
+
+##
+## Runas alias specification
+##
+
+##
+## User privilege specification
+##
+root ALL=(ALL) ALL
+
+## Uncomment to allow members of group wheel to execute any command
+%wheel ALL=(ALL) ALL
+
+## Same thing without a password
+# %wheel ALL=(ALL) NOPASSWD: ALL
+
+## Uncomment to allow members of group sudo to execute any command
+# %sudo ALL=(ALL) ALL
+
+## Uncomment to allow any user to run sudo if they know the password
+## of the user they are running the command as (root by default).
+# Defaults targetpw  # Ask for the password of the target user
+# ALL ALL=(ALL) ALL  # WARNING: only use this together with 'Defaults targetpw'
+
+%rfkill ALL=(ALL) NOPASSWD: /usr/sbin/rfkill
+%network ALL=(ALL) NOPASSWD: /usr/bin/netcfg, /usr/bin/wifi-menu
+
+## Read drop-in files from /etc/sudoers.d
+## (the '#' here does not indicate a comment)
+#includedir /etc/sudoers.d
+EOF
+
+    chmod 440 /etc/sudoers
+}
+
+set_root_password() {
+    local password="$1"; shift
+
+    echo -en "$password\n$password" | passwd
+}
+
+create_user() {
+    local name="$1"; shift
+    local password="$1"; shift
+
+    useradd -m -s /bin/zsh -G adm,systemd-journal,wheel,rfkill,games,network,video,audio,optical,floppy,storage,scanner,power,adbusers,wireshark "$name"
+    echo -en "$password\n$password" | passwd "$name"
+}
+
+update_locate() {
+    updatedb
+}
+
+get_uuid() {
+    blkid -o export "$1" | grep UUID | awk -F= '{print $2}'
+}
+
+set -ex
+
+if [ "$1" == "chroot" ]
+then
+    configure
+else
+    setup
+fi

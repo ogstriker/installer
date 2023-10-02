f9e20c43485a26194096f98e941a6af4ff44c513
ogstriker <146384997+ogstriker@users.noreply.github.com>
2023-10-02 20:47:38 -0300

    dw

--- arch_install.sh
+++ arch_install.sh
@@ -188,7 +188,7 @@ install_packages() {
     fi
 
     # Java stuff
-#    packages+=' jdk17-openjdk jre17-openjdk'
+    packages+=' jdk17-openjdk jre17-openjdk'
 
     # Libreoffice
 #    packages+=' libreoffice-calc libreoffice-en-US'
@@ -204,8 +204,9 @@ install_packages() {
 
     # XFCE desktop
 #    packages+=' xfce xfce-goodies nm-connection-editor nm-tray'
+
     # Wine and games dependencies
-#    packages=+' gamescope gamemode lib32-gamemode wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader'
+    packages=+' gamescope gamemode lib32-gamemode wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader'
 
     # Xserver
     packages+=' xorg-apps xorg-server xorg-xinit xterm'
